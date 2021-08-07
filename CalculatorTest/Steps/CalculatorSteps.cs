9c99a07896becb0e0a0fb1ecd27bcaf81af1bcbf
Jan Martin San Miguel <78434@grundfos.com>
2021-08-07 15:15:02 +0800

    trying library

--- CalculatorTest/Steps/CalculatorSteps.cs
+++ CalculatorTest/Steps/CalculatorSteps.cs
@@ -2,6 +2,7 @@
 using CalculatorTest.Pages;
 using System;
 using TechTalk.SpecFlow;
+using Tesseract;
 
 namespace CalculatorTest.Steps
 {
@@ -9,22 +10,35 @@ namespace CalculatorTest.Steps
     public class CalculatorSteps
     {
         CalculatorPage _calculatorPage;
+        Helper _helper;
 
         public CalculatorSteps()
         {
             _calculatorPage = new CalculatorPage();
+            _helper = new Helper();
         }
 
         [Given(@"the first number is (.*)")]
         public void GivenTheFirstNumberIs(int p0)
         {
             _calculatorPage.hhhhhmmm();
+            _helper.takeScreenShot("Result");
         }
         
         [Given(@"the second number is (.*)")]
         public void GivenTheSecondNumberIs(int p0)
         {
-
+            //var ocrengine = new TesseractEngine(@".\tessdata", "eng", EngineMode.Default);
+            //var ocrengine = new TesseractEngine(@"C:\Users\78434\source\repos\CalculatorTest\CalculatorTest\bin\Debug\tessdata", "eng", EngineMode.Default);
+            var ocrengine = new TesseractEngine(@"C:\Users\78434\source\repos\CalculatorTest\CalculatorTest\bin\Debug\tessdata", "eng");
+            //var img = Pix.LoadFromFile(@"C:\Users\78434\source\repos\CalculatorTest\TestResults\87202123821PM\9.png");
+            var img = Pix.LoadFromFile(@"C:\Users\78434\Downloads\tesseract-samples-master\src\Tesseract.ConsoleDemo\phototest.tif");
+        
+             var res = ocrengine.Process(img);
+            var a = res.GetText();
+            ocrengine.Dispose();
+            //Console.WriteLine(res.GetText());
+            //Console.ReadKey();
         }
         
         [When(@"the two numbers are added")]
